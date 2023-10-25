8ec060d20d76cbd68395079ecf945b168fc17946
Dilshad <134768995+ShadBalti@users.noreply.github.com>
2023-10-25 14:40:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Function to handle file input change
+  const FileRead = () => {
+  let reader = new FileReader();
+  let fileInput = document.getElementById('file');
+  let img = document.getElementById('img');
+
+  fileInput.addEventListener('change', function () {
+    if (this.files && this.files[0]) {
+      let file = this.files[0];
+      reader.onload = function (e) {
+        img.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+}
+
+  }
+
+  // Get references to HTML elements
+  let img = document.getElementById('img');
+  let zoom = document.getElementById('zoom');
+  let opacity = document.getElementById('opacity');
+  let blur = document.getElementById('blur');
+  let invert = document.getElementById('invert');
+  let contrast = document.getElementById('contrast');
+
+  let span = document.querySelectorAll('.arrow');
+  let arrUp = span[0];
+  let arrRe = span[1];
+  let arrLe = span[2];
+  let arrDw = span[3];
+
+  let rotateButton = document.getElementById('rotate');
+
+  // Add event listeners for various image adjustments
+  zoom.addEventListener('input', () => {
+    let valz = zoom.value;
+    img.style.filter = `brightness(${valz})`;
+    document.getElementById('sp1').textContent = valz;
+  });
+
+  opacity.addEventListener('input', () => {
+    let valo = opacity.value;
+    img.style.opacity = valo / 100;
+    document.getElementById('sp2').textContent = valo;
+  });
+
+  blur.addEventListener('input', () => {
+    let valz = blur.value;
+    img.style.filter = `blur(${valz}px)`;
+    document.getElementById('spBlur').textContent = valz;
+  });
+
+  contrast.addEventListener('input', () => {
+    let valz = contrast.value;
+    img.style.filter = `contrast(${valz})`;
+    document.getElementById('spCon').textContent = valz;
+  });
+
+  invert.addEventListener('input', () => {
+    let valo = '0.' + invert.value;
+    img.style.filter = `invert(${valo})`;
+    document.getElementById('spin').textContent = invert.value;
+  });
+
+  // Add event listeners for arrow buttons
+  arrUp.addEventListener('click', () => {
+    let hi = img.clientHeight + 4;
+    img.style.height = hi + 'px';
+  });
+
+  arrDw.addEventListener('click', () => {
+    let hi = img.clientHeight - 4;
+    img.style.height = hi + 'px';
+  });
+
+  arrLe.addEventListener('click', () => {
+    let hi = img.clientWidth + 4;
+    img.style.width = hi + 'px';
+  });
+
+  arrRe.addEventListener('click', () => {
+    let hi = img.clientWidth - 4;
+    img.style.width = hi + 'px';
+  });
+
+  // Add event listener for image rotation
+  let currentRotation = 0; // Initialize the current rotation angle
+
+  rotateButton.addEventListener('click', () => {
+    currentRotation += 90;
+    img.style.transform = `rotate(${currentRotation}deg)`;
+  });
+});
